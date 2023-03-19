470ab9dd312f0803ab8e17065d21ca7479cd261e
MikiHoffmann <mh.miki.hoffmann@gmail.com>
2023-03-19 14:42:16 +0100

    active/inactive link on navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,16 @@
 import "./NavBar.scss"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { NepflixLogo } from "../Logos/NepflixLogo"
 import { useContext } from "react"
 import { LangContext } from "../../redux/language/languageContext"
 import { useState } from "react"
+import { useLocation } from "react-router-dom"
 import SearchBar from "./SearchBar"
 import { IconNotification } from "../Icons/IconNotification"
 import { NepflixProfileImg } from "../Icons/Nepflix-profile-img"
 import { IconCaretDown } from "../Icons/IconCaretDown"
 import { IconPencil } from "../Icons/IconPencil"
 import { IconAccount } from "../Icons/IconAccount"
-
 const NavBar = () => {
   const { language } = useContext(LangContext)
   const [isScrolled, setIsScrolled] = useState(false)
@@ -18,6 +18,9 @@ const NavBar = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true)
     return () => (window.onscroll = 0)
   }
+  const location = useLocation()
+  const { pathname } = location
+  const splitPathname = pathname.split("/")
 
   return (
     <>
@@ -31,18 +34,18 @@ const NavBar = () => {
         </Link>
         <div className="navBarLinkContainer">
           <div className="pageLinkContainer">
-            <Link to="/home">
+            <NavLink to="/home" className={splitPathname[1] === "home" ? "activeLink" : ""}>
               <span>{language === "EN" ? "Homepage" : "Homepagina"}</span>
-            </Link>
-            <Link to="/movies">
+            </NavLink>
+            <NavLink to="/movies" className={splitPathname[1] === "movies" ? "activeLink" : ""}>
               <span>{language === "EN" ? "Movies" : "Films"}</span>
-            </Link>
-            <Link to="/series">
+            </NavLink>
+            <NavLink to="/series" className={splitPathname[1] === "series" ? "activeLink" : ""}>
               <span>Series</span>
-            </Link>
-            <Link to="/mylist">
+            </NavLink>
+            <NavLink to="/mylist" className={splitPathname[1] === "mylist" ? "activeLink" : ""}>
               <span>{language === "EN" ? "My list" : "Mijn lijst"}</span>
-            </Link>
+            </NavLink>
           </div>
           <div className="menuContainer">
             <SearchBar />
